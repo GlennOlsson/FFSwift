b44692c80842dff164e23d84c3c0e0367c8312a5
Glenn Olsson <glennholsson@gmail.com>
2023-07-04 20:42:30 +0200

    Create FFS class

--- Sources/FFSwift/Filesystem/Filesystem.swift
+++ Sources/FFSwift/Filesystem/Filesystem.swift
@@ -1,28 +1,68 @@
 import Foundation
 
-//MARK: All filesystem operation functions
+// MARK: All filesystem operation functions
 
-func open(_ path: String, _ flags: Int32) -> Int32 {
-    // Implementation
-	return 0
-}
+class FFS {
 
-func close(_ fd: Int32) -> Int32 {
-    // Implementation
-	return 0
-}
+	let fileHandler: FileHandler
+	let state: FilesystemState
 
-func write(_ fd: Int32, _ buf: UnsafeRawPointer, _ count: Int) -> Int {
-    // Implementation
-	return 0
-}
+	init(password: String) {
+		self.state = FilesystemState(password: password)
+		self.fileHandler = FileHandler(state: self.state)
+	}
 
-func read(_ fd: Int32, _ buf: UnsafeMutableRawPointer, _ count: Int) -> Int {
-    // Implementation
-	return 0
-}
+	/// Opens file at path and returns file descriptor
+	func open(_ path: String, _ flags: Int32) -> Int32 {
+		// Implementation
+		return 0
+	}
 
-func creat(_ path: String, _ mode: mode_t) -> Int32 {
-    // Implementation
-	return 0
+	/// Closes file descriptor
+	func close(_: Int32) -> Int32 {
+		// Implementation
+		return 0
+	}
+
+	/// Writes count bytes from buf to the file descriptor fd. Returns the number of bytes written
+	func write(_: Int32, _: UnsafeRawPointer, _: Int) -> Int {
+		// Implementation
+		return 0
+	}
+
+	/// Reads count bytes from the file descriptor fd into the buffer buf. Returns the number of bytes read
+	func read(_: Int32, _: UnsafeMutableRawPointer, _: Int) -> Int {
+		// Implementation
+		return 0
+	}
+
+	/// Creates a new file with the specified name and mode. Returns 0 on success, non-zero on failure
+	func creat(_: String, _: mode_t) -> Int32 {
+		// Implementation
+		return 0
+	}
+
+	/// Creates a new directory with the specified name and mode. Returns 0 on success, non-zero on failure
+	func mkdir(_: String, _: mode_t) -> Int32 {
+		// Implementation
+		return 0
+	}
+
+	/// Deletes a file at path from the filesystem. Returns 0 on success, non-zero on failure
+	func unlink(_: String) -> Int32 {
+		// Implementation
+		return 0
+	}
+
+	/// Deletes a directory at path from the filesystem. Returns 0 on success, non-zero on failure
+	func rmdir(_: String) -> Int32 {
+		// Implementation
+		return 0
+	}
+
+	/// Changes the name of a file from old to new. Returns 0 on success, non-zero on failure
+	func rename(_: String, _: String) -> Int32 {
+		// Implementation
+		return 0
+	}
 }
